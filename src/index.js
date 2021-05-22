59b63e7c8338eb5307d4c9ede4e0e3e02d8e0be7
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-05-22 14:01:23 -0300

    MOD.: Configuring CI for Heroku

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cors());    //Uso do módulo de segurança
 app.use(routes); //Uso das rotas da aplicação
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);

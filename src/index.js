9874b88313aad8c8b7453c784f812395b4fb36f7
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-05-23 01:54:28 -0300

    MOD.: Adding cors to API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
